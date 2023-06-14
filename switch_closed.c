c2afb1d91cb29f0913c8c80e6df43771db38aca6
Sakthi Priya K <127125636+SakthiPriya08@users.noreply.github.com>
2023-06-14 18:13:47 +0530

    Update switch_closed.c

--- switch_closed.c
+++ switch_closed.c
@@ -1,7 +1,7 @@
 #include <AT89S52.h>   /*include header for AT89S52*/
 #define LED P2_4   /*LED connected at port pin P2.4*/
 #define ON 0  /*logic 0 active */
-#define OFF 1  /*logic 1   active*/
+#define OFF 1  /*logic 1   inactive*/
 #define SW P3_3  /*switch connected at port pin P3.3*/
 #define INPUT 1  /*conf input as 1*/
 void main()
