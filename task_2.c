41314152ada5f722b64050c9928fcd5abee83926
Sakthi Priya K <127125636+SakthiPriya08@users.noreply.github.com>
2023-06-15 17:54:10 +0530

    Create task_2.c

---
+++ task_2.c
@@ -0,0 +1,27 @@
+#include<AT89S52.h>
+void main()
+{
+  char n;
+  LED=OFF;  /* Turn OFF the LED */
+  for(n=0;n<10;n++)
+  {
+   LED=ON;
+   wait(1);
+   LED=OFF;
+   wait(1);
+  }
+  while(1)  /* Execute below lines in super loop */
+  {
+  }
+}
+
+void wait(int del)  /* this program generate delay in multiples of 100us */
+{
+ int c1,c2;  /* Inner and outer loop Count variables */
+ for(c1=0; c1<del; c1++)	/* Outer loop runs for "del" times */
+ {
+  for(c2=0; c2<15; c2++)  /* Inner loop runs for 15 times */
+  {
+  }
+ }
+}
