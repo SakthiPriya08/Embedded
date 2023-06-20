fb8c9e03026b0132ca6092b0a670ef2a2c89401c
Sakthi Priya K <127125636+SakthiPriya08@users.noreply.github.com>
2023-06-20 17:35:48 +0530

    Update sw1orsw2_ledon.c

--- sw1orsw2_ledon.c
+++ sw1orsw2_ledon.c
@@ -17,3 +17,35 @@ void main()
 }
 	
 	}
+
+
+ANOTHER METHOD:
+
+
+#include <AT89S52.h> 
+#define LED P2_0  /*define the led*/
+#define SW1 P2_1  /*define the sw1*/
+#define SW2 P2_2  /*define the sw2*/
+#define ON 0      /*define the on*/
+#define OFF 1     /*define the off*/
+#define INPUT 1
+void main()
+{
+	SW1=INPUT;
+	SW2=INPUT;
+	LED=OFF; /*initially led off*/
+	while(1)
+	{
+	__bit a,b;
+	a=SW1;
+	b=SW2;
+	if(a==0||b==0)
+          LED=ON;  /*led is on*/
+          else
+          LED=OFF;  /*led is off*/
+   
+}
+	
+	}
+
+
