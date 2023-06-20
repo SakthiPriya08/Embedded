865f956b6e6c06a8d969ee4b353364059d9e320a
Sakthi Priya K <127125636+SakthiPriya08@users.noreply.github.com>
2023-06-20 17:20:57 +0530

    Create sw1andsw2_ledon.c

---
+++ sw1andsw2_ledon.c
@@ -0,0 +1,19 @@
+#include <AT89S52.h> 
+#define LED P2_0  /*define the led*/
+#define SW1 P2_1  /*define the sw1*/
+#define SW2 P2_2  /*define the sw2*/
+#define ON 0      /*define the on*/
+#define OFF 1     /*define the off*/
+void main()
+{
+	LED=OFF; /*initially led off*/
+	while(1)
+	{
+	if(SW1==0&&SW2==0)
+          LED=ON;  /*led is on*/
+          else
+          LED=OFF;  /*led is off*/
+   
+}
+	
+	}
