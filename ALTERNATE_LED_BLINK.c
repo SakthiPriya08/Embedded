7cfc39f51283a8e1998699727c502ef0c6a77327
Sakthi Priya K <127125636+SakthiPriya08@users.noreply.github.com>
2023-06-22 17:59:53 +0530

    Create ALTERNATE_LED_BLINK.c

---
+++ ALTERNATE_LED_BLINK.c
@@ -0,0 +1,34 @@
+#include<AT89S52.h>
+void wait(int);
+void main()
+{
+	char data=0xAA;
+	while(1)
+	{
+	P0=0X01;
+	wait(1);
+	P0=0X02;
+	wait(1);
+	P0=0X04;
+	wait(1);
+	P0=0X08;
+	wait(1);
+	P0=0X10;
+	wait(1);
+	P0=0X20;
+	wait(1);
+	P0=0X40;
+	wait(1);
+	P0=0X80;
+	wait(1);
+	}
+}
+void wait(int del)
+{
+	int i,j;
+	for(i=0;i<del;i++);
+	{
+	for(j=0;j<15;j++){
+	}
+	}
+}
